8a79edae773c2d66b089312d31eaad9de84b1e93
Khashayar Shomali <khashi792017@gmail.com>
2023-07-14 17:48:46 +0330

    Send data from parent to child with Props.

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <div class="Hello">
+    <h1>Hello, I am Child Component.</h1>
+  </div>
+
+  <div class="message">
+    <h2>{{ message }}</h2>
+  </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from "vue";
+
+const props = defineProps({
+  message: String,
+});
+</script>
+
+<style scoped>
+.message {
+  background-color: rgb(173, 255, 221);
+  height: 100px;
+}
+</style>
