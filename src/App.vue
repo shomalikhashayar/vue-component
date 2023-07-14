a267282cc2444456de7cb6920d17383624aa41e2
Khashayar Shomali <khashi792017@gmail.com>
2023-07-14 19:21:11 +0330

    Pass data to parent from child with emit functions.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <child-component message="This is message comes from parrent....">
+  <child-component
+    @change-bg-red="changeBackgroundToRed"
+    message="This is message comes from parrent...."
+  >
     <template #changeBackgroundToRed>
-      <button>Change bg to red!</button>
+     <H1>This template goes to child from parent by slots</H1>
     </template>
   </child-component>
 </template>
 
 <script setup>
-import ChildComponent from "./components/ChildComponent.vue";
+import ChildComponent from "./components/ChildComponent.vue"
+
+function changeBackgroundToRed() {
+  document.body.style.background = 'red';
+}
 </script>
 
 <style lang="scss" scoped></style>
--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="">
     <div class="Hello">
-    <h1>Hello, I am Child Component.</h1>
-  </div>
+      <h1>Hello, I am Child Component.</h1>
+       <button @click="$emit('change-bg-red')">Change bg to red!</button>
+    </div>
 
-  <div class="message">
-    <h2>{{ message }}</h2>
-  </div>
-  <slot name="changeBackgroundToRed"/>
+    <div class="message">
+      <h2>{{ message }}</h2>
+    </div>
+    <slot name="changeBackgroundToRed" />
   </div>
 </template>
 
 <script setup>
-import { ref, defineProps } from "vue";
+import { ref, defineProps, defineEmits } from "vue";
 
 const props = defineProps({
   message: String,
 });
+
+const emit = defineEmits(["change-bg-red"]);
+
 </script>
 
 <style scoped>
